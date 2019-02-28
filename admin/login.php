c07684bd2ccfd127a8ed311fc3b8b9fa3f2a5f1a
Sergey A. Chernyaev <chernyaev_sergei@mail.ru>
2019-02-28 19:34:54 +0500

    LOGIN Full WORK

--- admin/login.php
+++ admin/login.php
@@ -1,6 +1,17 @@
 <?php
+	//Example /admin/login.php?login=LOGIN&pass=PASSSHA1
 	include_once "../function.php";
 	$login = $_REQUEST['login'];
 	$pass = $_REQUEST['pass'];
-	print($login + ' ' + $pass);
+	$link = db_connect();
+	if($link){
+		$sqltext="SELECT IDPost, Login, PassSHA1 FROM Workers WHERE Login = '".$login."' AND PassSHA1 = '".$pass."'";
+		$query=sqlsrv_query($link, $sqltext);
+		$array=sqlsrv_fetch_array($query);
+		
+		if($array==NULL)
+			print('Not found');
+		else
+			print($array[0]);
+	}
 ?>
