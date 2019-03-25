6fb5af8115dd049d786abd5cf0b67102f5612e50
sergeigeimer <sergeigeimerchat@gmail.com>
2019-03-25 21:44:52 +0500

    Получение и поиск записей из справочника населённых пунктов locality\get.php

---
+++ turoperator/locality/get.php
@@ -0,0 +1,7 @@
+<?php
+	//Example /turoperator/locality/get.php?paramName=PARANAME&paramValue=PARAMVALUE + login string
+	$table = "Locality";
+	$post = "Туроператор";
+	include_once "../../general/get.php";
+	
+?>
