a4198b635b6f95367c4486aca5d2ce5f64ad8393
sergeigeimer <sergeigeimerchat@gmail.com>
2019-03-25 21:57:53 +0500

    Получение и поиск записей из справочника виды транспорта transport/get.php

---
+++ turoperator/transport/get.php
@@ -0,0 +1,7 @@
+<?php
+	//Example /turoperator/transport/get.php?paramName=PARANAME&paramValue=PARAMVALUE + login string
+	$table = "TypeOfTransports";
+	$post = "Туроператор";
+	include_once "../../general/get.php";
+	
+?>
